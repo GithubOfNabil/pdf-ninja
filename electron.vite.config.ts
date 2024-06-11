08a45ec17878c0b2fd334516287cf6dfb435927c
Nabil <mailboxofnabil@gmail.com>
2024-06-12 00:17:12 +0600

    Tailwind setup

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -17,4 +18,6 @@ export default defineConfig({
     },
     plugins: [react()]
   }
+
+
 })
