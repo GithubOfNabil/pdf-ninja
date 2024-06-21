5abead9371ff2a3424652d33d403bc09431cdddb
Nabil <mailboxofnabil@gmail.com>
2024-06-22 03:03:08 +0600

    Add bg and Logo

--- src/renderer/src/components/Home.tsx
+++ src/renderer/src/components/Home.tsx
@@ -1,13 +1,21 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import Logo from "./Logo";
 
 export default function Home(): ReactElement {
 
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/image-to-pdf"><button>Image To PDF</button></Link>
-      <Link to="/pdf-split"><button>PDF Split</button></Link>
+    <div className=" h-screen p-4 bg-teal-900">
+      
+      <div className=" mb-4">
+      <h1 className=" text-teal-200 font-bold">Home</h1>
+      <div className=" flex justify-center items-end w-12 h-11 rounded-md bg-black " ><Logo className=" w-10 h-10"/></div>
+      </div>
+
+      <div className=" flex flex-row gap-2 ">
+      <div className=" text-center w-32 border-solid rounded-md p-2 bg-teal-300 text-black font-semibold"><Link to="/image-to-pdf">Image To PDF</Link></div>
+      <div className=" text-center w-32 border-solid rounded-md p-2 bg-teal-300 text-black font-semibold"><Link to="/pdf-split">PDF Split</Link></div>
+      </div>
     </div>
   )
 }
